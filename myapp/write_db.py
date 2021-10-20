6aa35733083091f324150a13f2e421abcea71ac0
Chandlergee <18483600758@163.com>
2021-10-20 14:19:21 +0800

    write db

--- myapp/write_db.py
+++ myapp/write_db.py
@@ -1,12 +1,17 @@
 import os,django
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','myapp.settings')
 django.setup()
 
 from videos.models import TVSeries,Episode
 
-x = TVSeries(tv_name='平凡的世界',tv_country = '华语',tv_mtype = '家庭',tv_description = '略')
-x.save()
-
+with open('../tools/data.json','r',encoding='utf-8') as fd:
+    import json
+    eps = json.loads(fd.read())
+    tv = TVSeries.objects.get(id = 3)
+    for item in eps:
+        ep = Episode(tv_id = tv,e_title = item['name'],e_url = item['href'])
+        ep.save()
 
 
