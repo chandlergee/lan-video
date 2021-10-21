2ad1d4d390440f1233c0e7e046a8e80de7ff1f9c
chandlergee <18483600758@163.com>
2021-10-21 12:09:40 +0800

    add route

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
+    path('',include('videos.urls')),
     path('admin/', admin.site.urls),
     path('videos/',include('videos.urls'))
 ]
