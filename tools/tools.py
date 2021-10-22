5116e124152adfdaefaab456c11a675c607a07e1
chandlergee <18483600758@163.com>
2021-10-22 23:35:37 +0800

    增加数据

--- tools/tools.py
+++ tools/tools.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import json
 
-WRITE_FILE_PATH = 'data.json'
+WRITE_FILE_PATH = 'data3.json'
 READ_FILE_PATH = '1.html'
 
 def load_html(path,encoding='utf-8'):
@@ -16,7 +16,7 @@ href_list = []
 soup = BeautifulSoup(load_html(READ_FILE_PATH))
 for item in soup.find_all('a'):
     x = dict()
-    x['name'] = '平凡的世界 第{}集'.format(str(item.contents[0]).strip())
+    x['name'] = '爱情公寓第五季 第{}集'.format(str(item.contents[0]).strip())
     x['href'] = 'https://v.qq.com/' + item['href']
     href_list.append(x)
 
